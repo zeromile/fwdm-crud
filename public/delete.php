338ebdce5d9b95b1a33850a1c5e927279f2f6b8a
zeromile <jasonleecooksey@gmail.com>
2018-01-25 16:56:34 -0800

    blank out files for editing

--- public/delete.php
+++ public/delete.php
@@ -1,11 +1,2 @@
 <?php
 require_once "../connect.php";
-
-$id = $_GET['id'];
-$DelSql = "DELETE FROM `crud` WHERE id=$id";
-$res = mysqli_query($connection, $DelSql);
-if($res){
-	header('location: read.php');
-}else{
-	echo "Failed to delete";
-}
